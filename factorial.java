950645e0d5740aabf35efa5f233398c2c3c92842
Suhail8628 <135505489+Suhail8628@users.noreply.github.com>
2024-11-02 10:51:46 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,10 +1,10 @@
 class factorial { 
     static int factorial(int n) 
     { 
-        int res = 1, i; 
+        int fact = 1, i; 
         for (i = 2; i <= n; i++) 
-            res *= i; 
-        return res; 
+            fact *= i; 
+        return fact; 
     } 
     public static void main(String[] args) 
     { 
